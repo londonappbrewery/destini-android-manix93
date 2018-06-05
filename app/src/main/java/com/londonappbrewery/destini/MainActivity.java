5a8e73744862f956305c95823c7ccbeb848120d1
manix93 <manix93@gmail.com>
2018-06-05 16:34:07 +0200

    Completed

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,29 +2,114 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO: Steps 4 & 8 - Declare member variables here:
-
+    TextView mStoryText;
+    Button mTopButton;
+    Button mBottomButton;
+    int mCurrentLevel;
+    int mCurrentState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mStoryText = findViewById(R.id.storyTextView);
+        mTopButton = findViewById(R.id.buttonTop);
+        mBottomButton = findViewById(R.id.buttonBottom);
+        mCurrentLevel = 0;
+        mCurrentState = 0;
 
-        // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
+        mTopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setStory("top");
+            }
+        });
 
+        mBottomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setStory("bottom");
+            }
+        });
+    }
 
+    private void setStory(String choose)
+    {
+        if(mCurrentLevel == 0)
+        {
+            if(choose == "top")
+            {
+                mStoryText.setText(R.string.T3_Story);
+                mTopButton.setText(R.string.T3_Ans1);
+                mBottomButton.setText(R.string.T3_Ans2);
+            }
 
-        // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
+            else if(choose == "bottom")
+            {
+                mStoryText.setText(R.string.T2_Story);
+                mTopButton.setText(R.string.T2_Ans1);
+                mBottomButton.setText(R.string.T2_Ans2);
+                mCurrentState = 1;
+            }
+        }
 
+        else if(mCurrentLevel == 1)
+        {
+            if(mCurrentState == 0)
+            {
+                if(choose == "top")
+                {
+                    mStoryText.setText(R.string.T6_End);
+                    mTopButton.setVisibility(View.INVISIBLE);
+                    mBottomButton.setVisibility(View.INVISIBLE);
+                }
 
+                else if(choose == "bottom")
+                {
+                    mStoryText.setText(R.string.T5_End);
+                    mTopButton.setVisibility(View.INVISIBLE);
+                    mBottomButton.setVisibility(View.INVISIBLE);
+                }
+            }
 
+            else if(mCurrentState == 1)
+            {
+                if(choose == "top")
+                {
+                    mStoryText.setText(R.string.T3_Story);
+                    mTopButton.setText(R.string.T3_Ans1);
+                    mBottomButton.setText(R.string.T3_Ans2);
+                }
 
-        // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
+                else if(choose == "bottom")
+                {
+                    mStoryText.setText(R.string.T4_End);
+                    mTopButton.setVisibility(View.INVISIBLE);
+                    mBottomButton.setVisibility(View.INVISIBLE);
+                }
+            }
+        }
 
+        else if(mCurrentLevel == 2) {
 
+            if (choose == "top") {
+                mStoryText.setText(R.string.T6_End);
+                mTopButton.setVisibility(View.INVISIBLE);
+                mBottomButton.setVisibility(View.INVISIBLE);
+            } else if (choose == "bottom") {
+                mStoryText.setText(R.string.T5_End);
+                mTopButton.setVisibility(View.INVISIBLE);
+                mBottomButton.setVisibility(View.INVISIBLE);
+            }
+        }
+
+        mCurrentLevel++;
     }
 }
